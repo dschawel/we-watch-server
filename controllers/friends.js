f82d5095f07f2f3b32ad642bbf3fdc86f4d9588e
dschawel <david.schawel@gmail.com>
2020-02-18 20:43:25 -0800

    I think I got the search for a friend kinda working

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,30 +1,28 @@
 let db = require('../models')
 let router = require('express').Router()
 
-router.get('/search', (req, res) => {
-    db.User.find({ firstname: req.user.firstname })
-    if (!user) {
-        res.status(404).send({ message: 'No one matches that name'})
-    } else {
-        res.status(200).send(user)
-    }
-})
+// router.get('/search', (req, res) => {
+//     console.log('Hitting the get route for search?')
+//     db.User.find({ firstname: req.user.firstname })
+//     if (!user) {
+//         res.status(404).send({ message: 'No one matches that name'})
+//     } else {
+//         res.status(200).send(user)
+//     }
+// })
 
 router.post('/search', (req, res) => {
-    let searchFriend = req.body.searchFriend
-    if (searchFriend == req.user.firstname) {
-        searchFriend = null
-    }
-    db.User.find({ firstname: searchFriend })
-    if (!user) {
+    console.log('Hitting the post route for search?')
+    let searchFriend = req.user.firstname
+    // if (searchFriend == req.user.firstname) {
+    //     searchFriend = null
+    // }
+    db.User.findOne({ searchFriend })
+    if (!searchFriend) {
         res.status(404).send({ message: 'No one matches that name'})
     } else {
-        res.status(200).send(user)
+        res.status(200).send(searchFriend)
     }
 })
-
-
-
-
 
 module.exports = router
