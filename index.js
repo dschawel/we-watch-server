ed46bb8ea148e647333b275a30b44f6810458413
dschawel <david.schawel@gmail.com>
2020-10-30 17:38:26 -0700

    added something about cors

--- index.js
+++ index.js
@@ -26,10 +26,14 @@ let app = express()
 //   });
 
 let rowdyResults = rowdyLogger.begin(app)
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
 
 // Set up middleware
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: false })) // Accept form data
 app.use(express.json()) // Accept data from fetch (or any AJAX call)
 
